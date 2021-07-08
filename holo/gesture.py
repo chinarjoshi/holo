50797609976bedc66266dc354c1855ef89b17fad
Chinar Joshi <chinarjoshi7@gmail.com>
2021-07-08 08:13:59 -0400

    Finished modularization of functions

--- holo/gesture.py
+++ holo/gesture.py
@@ -22,7 +22,7 @@ def get_model(path: str = "../models/VGG_cross_validated.h5") -> keras.Model:
 # Open palm for expand, close palm for retract, ok for right, peace for left, L for rotate up and down.
 
 
-def predict_image(image: np.ndarray, model: keras.Model) -> np.int64:
+def predict_gesture(image: np.ndarray, model: keras.Model) -> np.int64:
     """Returns gesture result and confidence given cv2 image.
 
     Formats a given image to fit tensorflow image size requireemnts, takes
@@ -53,29 +53,43 @@ def gesture_names(gestures: tuple = ("Fist", "L", "Okay", "Palm", "Peace")) -> d
 def map_gestures(confidence_index: np.int64, gesture_names: dict) -> str:
     """Returns gesture type and confidence given max index from confidence array."""
     result = gesture_names[confidence_index]
-    score = float("%0.2f" % (max(pred_array[0]) * 100))
-    return result, score
+    # score = float("%0.2f" % (max(pred_array[0]) * 100))
+    return result, 15
+
+
+def process_frame(image: np.ndarray = cv2.imread("../images/L.jpg")) -> np.ndarray:
+    frame = cv2.resize(image, (224, 224))
+    frame = frame.reshape(1, 224, 224, 3)
+    return frame
 
 
 def prediction_from_camera():
     """Enables webcam and returns transformation type and confidence."""
-    camera = cv2.VideoCapture(0)
+    # camera = cv2.VideoCapture(0)
+    # while camera.isOpened():
+    #     # ret returns True if camera is running, frame grabs each frame of the video feed
+    #     running, frame = camera.read()
 
-    while camera.isOpened():
-        # ret returns True if camera is running, frame grabs each frame of the video feed
-        running, frame = camera.read()
+    #     if not running:
+    #         break
 
-        if not running:
-            break
+    #     frame = np.stack((frame,) * 3, axis=-1)
+    #     frame = cv2.resize(frame, (224, 224))
+    #     frame = frame.reshape(1, 224, 224, 3)
+    #     prediction, score = predict_image(frame, gesture_names, get_model())
 
-        frame = np.stack((frame,)*3, axis=-1)
+    #     # Wait for 100 milliseconds between iterations to reduce processing strain
+    #     time.sleep(0.1)
+    # else:
 
-    else:
-        frame = cv2.imread("../images/L.jpg")
+    for root, dirs, files in os.walk("../images", topdown=False):
+        for name in files:
+            print(os.path.join(root, name))
 
-        frame = cv2.resize(frame, (224, 224))
-        frame = frame.reshape(1, 224, 224, 3)
-        prediction, score = predict_image(frame, gesture_names, get_model())
+    prediction_index = predict_gesture(image=process_frame(), model=get_model())
+    print(
+        map_gestures(confidence_index=prediction_index, gesture_names=gesture_names())
+    )
 
 
 if __name__ == "__main__":
