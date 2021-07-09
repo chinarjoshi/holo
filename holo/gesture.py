eafe33df507e00fc0e7c2b470613a797adfcd9bb
Chinar Joshi <chinarjoshi7@gmail.com>
2021-07-09 05:40:51 -0400

    Overhaul gesture control to video capture rather than static image

--- holo/gesture.py
+++ holo/gesture.py
@@ -1,6 +1,7 @@
 import os
 from dataclasses import dataclass
 
+import time
 import cv2
 import numpy as np
 from keras.models import load_model
@@ -11,6 +12,9 @@ from tensorflow import keras
 class GestureOutput:
     gesture: str
     confidence: int
+
+    def __repr__():
+        return f'gesture: {self.gesture}, confidence: {self.confidence}%'
 
 
 def get_model(path: str = "../models/VGG_cross_validated.h5") -> keras.Model:
@@ -25,9 +29,6 @@ def get_model(path: str = "../models/VGG_cross_validated.h5") -> keras.Model:
         return load_model("../models/VGG_cross_validated.h5")
     else:
         raise Exception(f"Model not found in relative path: {path}")
-
-
-# Open palm for expand, close palm for retract, ok for right, peace for left, L for rotate up and down.
 
 
 def predict_gesture(image: np.ndarray, model: keras.Model) -> np.int64:
@@ -73,7 +74,8 @@ def map_gestures(confidence_array: np.array, gesture_names: dict) -> tuple:
     return GestureOutput(gesture, confidence)
 
 
-def process_frame(image: np.ndarray = cv2.imread("../images/L.jpg")) -> np.ndarray:
+def process_frame(image: np.ndarray) -> np.ndarray:
+    frame = np.stack((image,) * 3, axis=-1)
     frame = cv2.resize(image, (224, 224))
     frame = frame.reshape(1, 224, 224, 3)
     return frame
@@ -81,29 +83,22 @@ def process_frame(image: np.ndarray = cv2.imread("../images/L.jpg")) -> np.ndarr
 
 def prediction_from_camera():
     """Enables webcam and returns transformation type and confidence."""
-    # camera = cv2.VideoCapture(0)
-    # while camera.isOpened():
-    #     # ret returns True if camera is running, frame grabs each frame of the video feed
-    #     running, frame = camera.read()
+    camera = cv2.VideoCapture(0)
+    model = get_model()
+    while True: #camera.isOpened():
+        # ret returns True if camera is running, frame grabs each frame of the video feed
+        ret, frame = camera.read()
+        # cv2.imshow('image', frame)
 
-    #     if not running:
-    #         break
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
 
-    #     frame = np.stack((frame,) * 3, axis=-1)
-    #     frame = cv2.resize(frame, (224, 224))
-    #     frame = frame.reshape(1, 224, 224, 3)
-    #     prediction, score = predict_image(frame, gesture_names, get_model())
-
-    #     # Wait for 100 milliseconds between iterations to reduce processing strain
-    #     time.sleep(0.1)
-    # else:
-
-    # for root, dirs, files in os.walk("../images", topdown=False):
-    #     for name in files:
-    #         print(os.path.join(root, name))
-
-    prediction_index = predict_gesture(image=process_frame(), model=get_model())
-    return(map_gestures(confidence_array=prediction_index, gesture_names=gesture_names()))
+        prediction_index = predict_gesture(image=process_frame(frame), model=model)
+        print(map_gestures(confidence_array=prediction_index, gesture_names=gesture_names()))
+        # time.sleep(.01)
+        # print(camera.isOpened())
+    camera.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     print(prediction_from_camera())
